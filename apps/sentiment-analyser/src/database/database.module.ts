72c2ebb2558abb35d3e9dfcb85bc601667b042b9
Onesmus Okonkwo <blessedOnesmus@gmail.com>
2025-05-12 10:44:10 +0100

    chore: boostrap nestjs app

--- apps/sentiment-analyser/src/database/database.module.ts
+++ apps/sentiment-analyser/src/database/database.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: false,
     }),
   ],
   exports: [],
