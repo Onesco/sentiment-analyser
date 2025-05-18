2d44d272c65814c808938ef23df280ae2f82d48f
Onesmus Okonkwo <blessedOnesmus@gmail.com>
2025-05-18 17:02:22 +0100

    ci: fix worker build

--- infra/terraform/modules/function/main.tf
+++ infra/terraform/modules/function/main.tf
@@ -6,7 +6,7 @@ data "google_storage_bucket" "source_bucket" {
 # Point at the existing object
 data "google_storage_bucket_object" "source_object" {
   bucket = data.google_storage_bucket.source_bucket.name
-  name   = "worker-${var.env_name}"
+  name   = "worker-${var.env_name}/${var.project_name}-artifact-${var.env_name}.zip"
 }
 
 # Deploy the Cloud Function
